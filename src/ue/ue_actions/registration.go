9b3bc5e0c0376fc9d00ccc396a1629a91f61170d
Matan Broner <matanbroner@gmail.com>
2022-02-08 10:08:37 -0600

    update ike bind addr

--- src/ue/ue_actions/registration.go
+++ src/ue/ue_actions/registration.go
@@ -1066,8 +1066,7 @@ func InitialRegistrationProcedure(ueContext *ue_context.UEContext) {
 			if responseConfiguration.ConfigurationType == message.CFG_REPLY {
 				for _, configAttr := range responseConfiguration.ConfigurationAttribute {
 					if configAttr.Type == message.INTERNAL_IP4_ADDRESS {
-						//ueAddr.IP = configAttr.Value
-						ueAddr.IP = net.ParseIP("192.168.56.102")
+						ueAddr.IP = configAttr.Value
 					}
 					if configAttr.Type == message.INTERNAL_IP4_NETMASK {
 						ueAddr.Mask = configAttr.Value
@@ -1125,7 +1124,9 @@ func InitialRegistrationProcedure(ueContext *ue_context.UEContext) {
 	}
 
 	if err := netlink.AddrAdd(linkIPSec, linkIPSecAddr); err != nil {
-		pingLog.Fatalf("Set ipsec0 addr failed: %v", err)
+		if !strings.Contains(fmt.Sprint(err), "file exists") {
+			pingLog.Fatal(err)
+		}
 	}
 
 	defer func() {
