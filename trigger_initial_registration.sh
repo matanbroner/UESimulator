06a6a5fae94d71680346f059900c502f217ab0cb
Matan Broner <matanbroner@gmail.com>
2022-04-04 20:12:22 -0500

    Change IP to 192.168.56.103

--- trigger_initial_registration.sh
+++ trigger_initial_registration.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ue_addr=${ue_addr:-"127.0.0.1"}
+ue_addr=${ue_addr:-"192.168.56.103"}
 ue_port=${ue_port:-"10000"}
 scheme=${scheme:-"http"}
 auth_method=${auth_method:-"5G_AKA"}
@@ -9,7 +9,7 @@ supi_or_suci=${supi_or_suci:-"2089300007487"}
 k=${k:-"5122250214c33e723a5dd523fc145fc0"}
 opc_type=${opc_type:-"OP"}
 opc=${opc:-"c9e8763286b5b9ffbdf56e1297d0887b"}
-ike_bind_addr=${ike_bind_addr:-"192.168.56.102"}
+ike_bind_addr=${ike_bind_addr:-"192.168.56.103"}
 
 while [ $# -gt 0 ]; do
    if [[ $1 == *"--"* ]]; then
